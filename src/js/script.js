91050c35d9774506f77b6d87dd1f1dd66ee5b146
Akhil Punia <ak216punia@gmail.com>
2023-01-14 01:03:49 +0530

    Fix scene width and height

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,18 @@ import * as THREE from '../../node_modules/three/build/three.module.js'
 
 const renderer = new THREE.WebGL1Renderer()
 
-renderer.setSize("200", "100")
+const screenWidth = window.innerWidth/2
+const screenHeight = window.innerHeight
+
+console.log(screenWidth)
+console.log(screenHeight)
+
+renderer.setSize(screenWidth,screenHeight)
 
 document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(45, 2, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, screenWidth/screenHeight, 0.1, 1000)
 
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
