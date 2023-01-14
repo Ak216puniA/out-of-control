62b411c12e51ea14110dd45019aba14fa166b7c5
Akhil Punia <ak216punia@gmail.com>
2023-01-14 16:27:21 +0530

    Make minor changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,15 @@
 const screenHeight =  window.innerHeight
 const screenWidth = window.innerWidth
 
-console.log(screenHeight)
-console.log(screenWidth)
+const gameWindowWidth = screenHeight/screenWidth<1.5 ? screenHeight/1.5 : screenWidth
+const gameWindowHeight = screenHeight/screenWidth<1.5 ? screenHeight : screenWidth*1.5
 
-if(true){
-    console.log("Entered")
-    const gameWindowWidth = screenHeight/screenWidth<1.5 ? screenHeight/1.5 : screenWidth
-    const gameWindowHeight = screenHeight/screenWidth<1.5 ? screenHeight : screenWidth*1.5
-    // document.body.style.width = gameWindowWidth
-    // document.body.style.height = gameWindowHeight
-    console.log(gameWindowWidth)
-    console.log(gameWindowHeight)
-    document.getElementById('gameWindow').style.width = `${gameWindowWidth}px`
-    document.getElementById('gameWindow').style.height = `${gameWindowHeight}px`
-}
+const gameWindow = document.getElementById('gameWindow')
 
 if(screenHeight<=25 || screenWidth<=25){
-    // console.log(document.body.width)
-    document.getElementById('gameWindow').style.width = `100%`
-    document.getElementById('gameWindow').style.height = `100vh`
+    gameWindow.style.width = `100vw`
+    gameWindow.style.height = `100vh`
+}else{
+    gameWindow.style.width = `${gameWindowWidth}px`
+    gameWindow.style.height = `${gameWindowHeight}px`
 }
