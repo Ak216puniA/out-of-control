7b0e52954445b3b690687237e11a1f5a8200d034
Akhil Punia <ak216punia@gmail.com>
2023-01-14 17:32:10 +0530

    Add elements inside start-page

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,31 @@ const gameWindowWidth = screenHeight/screenWidth<1.5 ? screenHeight/1.5 : screen
 const gameWindowHeight = screenHeight/screenWidth<1.5 ? screenHeight : screenWidth*1.5
 
 const gameWindow = document.getElementById('gameWindow')
+const headingText = document.getElementsByClassName('headingText')
+const normalText = document.getElementsByClassName('normalText')
+const playButton = document.getElementById('playButton')
 
 if(screenHeight<=25 || screenWidth<=25){
     gameWindow.style.width = `100vw`
     gameWindow.style.height = `100vh`
+
+    for(let i=0; i<headingText.length; i++){
+        headingText[i].style.fontSize = `17.4vw`
+    }
+    for(let i=0; i<normalText.length; i++){
+        normalText[i].style.fontSize = `13.9vw`
+    }
 }else{
     gameWindow.style.width = `${gameWindowWidth}px`
     gameWindow.style.height = `${gameWindowHeight}px`
+
+    const headingFontSize = gameWindowWidth*17.4/100
+    const normalFontSize = gameWindowWidth*13.9/100;
+
+    for(let i=0; i<headingText.length; i++){
+        headingText[i].style.fontSize = `${headingFontSize}px`
+    }
+    for(let i=0; i<normalText.length; i++){
+        normalText[i].style.fontSize = `${normalFontSize}px`
+    }
 }
